d4a974554b8ae5209b98c0506f1d874d8bdd64b3
mpal9000 <mpal@mail.org>
2015-01-20 22:16:31 +0200

    removed caching

--- index.js
+++ index.js
@@ -3,40 +3,6 @@
 var ip = require('ip-regex').v4().source;
 var TLDS = require('./tlds.json').join('|');
 
-var cache = { tld: null, url: null, exact: null };
-
-var auth = '(?:\\S+(?::\\S*)?@)?';
-var domain = '(?:\\.(?:xn--[a-z0-9\\-]{1,59}|(?:[a-z\\u00a1-\\uffff0-9]+-?){0,62}[a-z\\u00a1-\\uffff0-9]{1,63}))*';
-var host = '(?:xn--[a-z0-9\\-]{1,59}|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?){0,62}[a-z\\u00a1-\\uffff0-9]{1,63}))';
-var path = '(?:\/[^\\s]*)?';
-var port = '(?::\\d{2,5})?';
-var protocol = '(?:(?:(?:\\w)+:)?\/\/)?';
-
-function tld(tlds) {
-	tlds = tlds ? (tlds + '|' + TLDS) : TLDS;
-	return '(?:\\.(?:xn--[a-z0-9\\-]{1,59}|' + tlds + '+))';
-}
-
-function url(tld) {
-	return [
-		protocol + auth + '(?:' + ip + '|',
-		'(?:localhost)|' + host + domain + tld + ')' + port + path
-	].join('');
-}
-
-function regex(tlds) {
-	if (tlds && typeof tlds == 'object' && tlds.length) {
-		return url(tld(tlds.join('|')));
-	}
-
-	if (!cache.url) {
-		if (!cache.tld) cache.tld = tld();
-		cache.url = url(cache.tld);
-	}
-
-	return cache.url;
-}
-
 /**
  * Regular expression for matching URLs
  *
@@ -47,13 +13,27 @@ function regex(tlds) {
 module.exports = function (opts) {
 	opts = opts || {};
 
-	if (!opts.exact) {
-		return new RegExp('(?:^|\\s)(["\'])?' + regex(opts.additionalTlds) + '\\1', 'ig');
+	var tlds;
+	if (opts.additionalTlds
+	    && typeof opts.additionalTlds == 'object'
+	    && opts.additionalTlds.length) {
+		tlds = opts.additionalTlds.join('|') + '|' + TLDS;
 	}
+	else tlds = TLDS;
 
-	if (!cache.exact) {
-		cache.exact = new RegExp('(?:^' + regex(opts.additionalTlds) + '$)', 'i');
-	}
+	var auth = '(?:\\S+(?::\\S*)?@)?';
+	var domain = '(?:\\.(?:xn--[a-z0-9\\-]{1,59}|(?:[a-z\\u00a1-\\uffff0-9]+-?){0,62}[a-z\\u00a1-\\uffff0-9]{1,63}))*';
+	var host = '(?:xn--[a-z0-9\\-]{1,59}|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?){0,62}[a-z\\u00a1-\\uffff0-9]{1,63}))';
+	var path = '(?:\/[^\\s]*)?';
+	var port = '(?::\\d{2,5})?';
+	var protocol = '(?:(?:(?:\\w)+:)?\/\/)?';
+	var tld = '(?:\\.(?:xn--[a-z0-9\\-]{1,59}|' + tlds + '+))';
 
-	return cache.exact;
+	var regex = [
+		protocol + auth + '(?:' + ip + '|',
+		'(?:localhost)|' + host + domain + tld + ')' + port + path
+	].join('');
+
+	return opts.exact ? new RegExp('(?:^' + regex + '$)', 'i') :
+						new RegExp('(?:^|\\s)(["\'])?' + regex + '\\1', 'ig');
 };
